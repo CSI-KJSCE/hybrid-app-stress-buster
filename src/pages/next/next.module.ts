ded07bb451d5bc2f4312df2c1ff32f889a6fe324
Sarfaraz Ghulam Iraqui <sarfarazghlm@gmail.com>
2017-09-29 02:08:46 +0530

    new blank page added, v4

---
+++ src/pages/next/next.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { NextPage } from './next';
+
+@NgModule({
+  declarations: [
+    NextPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(NextPage),
+  ],
+})
+export class NextPageModule {}
